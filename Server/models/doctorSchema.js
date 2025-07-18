f855551c67bfec7e307bbaeb5303ca19b3f59d93
Muhammad Aman <amanmuhammad567@gmail.com>
2025-07-18 16:03:38 +0500

    Update doctorSchema.js

--- Server/models/doctorSchema.js
+++ Server/models/doctorSchema.js
@@ -1,87 +1,93 @@
-import mongoose from "mongoose"
-import bcrypt from "bcrypt"
-import jwt from "jsonwebtoken"
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 
-dotenv.config(); // make sure env variables are loaded
+dotenv.config();
 
 const DEFAULT_DP_URL = process.env.Default_DP;
 
-const doctorSchema= new mongoose.Schema({
-    email:{
-    type:String,
-    required:[true,"Email is required!!!"]
-    }, 
-    password:{
-    type:String,
-    required:[true,"Password is reuired!!!"]
-    },
-    fullname:{
-        type:String,
-        required : [true,"Full Name is required!!!"]
-    },
-    nic:{
-    type:String,
-    required:[true,"nic is required!!!"]
-    },
-    gender:{
-        type:String,
-        required:[true,"Gender Is Required!!!"]
-    },
-    dob:{
-        type:Date,
-        required:[true,"Date Of Birth Required!!!"]
-    },
-    phone:{
-        type:String,
-        required:[true,"Phone Number is reuired!!!"]
-    }, 
-    clinicaddress:{ 
-        type:String,
-        required:[true,"Clinic Address is required!!!"]
-    },
-    degree:{
-        type:String,
-        required:[true,"Degree is required"]
-    },
-    city:{
-        type:String,
-        required:[true,"City is required!!!"]
-    },
-    Experience:{
-        type:Number,
-        required:[true,"experience is required"]
-    },
-    bio: {
+const doctorSchema = new mongoose.Schema({
+  email: {
     type: String,
-    maxlength: 1000, 
+    required: [true, "Email is required!!!"]
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required!!!"]
+  },
+  fullname: {
+    type: String,
+    required: [true, "Full Name is required!!!"]
+  },
+  nic: {
+    type: String,
+    required: [true, "NIC is required!!!"]
+  },
+  gender: {
+    type: String,
+    required: [true, "Gender is required!!!"]
+  },
+  dob: {
+    type: Date,
+    required: [true, "Date of Birth is required!!!"]
+  },
+  phone: {
+    type: String,
+    required: [true, "Phone Number is required!!!"]
+  },
+  clinicaddress: {
+    type: String,
+    required: [true, "Clinic Address is required!!!"]
+  },
+  degree: {
+    type: String,
+    required: [true, "Degree is required!!!"]
+  },
+  city: {
+    type: String,
+    required: [true, "City is required!!!"]
+  },
+  Experience: {
+    type: Number,
+    required: [true, "Experience is required!!!"]
+  },
+  bio: {
+    type: String,
+    maxlength: 1000,
     default: ''
   },
-  role:{
-    type:String,
-    required:[true,"role is required"]
+  role: {
+    type: String,
+    required: [true, "Role is required!!!"]
   },
-  dpURL:{
-    type:String,
+  dpURL: {
+    type: String,
     default: DEFAULT_DP_URL
   }
-})
+});
 
-doctorSchema.pre("save",async function (next){
-if(!this.isModified("password")){
-    next();
-}
-this.password=await bcrypt.hash(this.password,10);
-})
+// Hash password before saving
+doctorSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
 
-doctorSchema.methods.comparePassword=async function (enteredpass){
-    return await bcrypt.compare(enteredpass,this.password);
-}
+// Compare entered password
+doctorSchema.methods.comparePassword = async function (enteredpass) {
+  return await bcrypt.compare(enteredpass, this.password);
+};
 
-doctorSchema.methods.generateToken=function(){
-    return jwt.sign({email:this.email},process.env.JWT_SECRET_KEY,{
-        expiresIn:process.env.JWT_EXPIRE  
-    })
-}   
+// Generate JWT
+doctorSchema.methods.generateToken = function () {
+  return jwt.sign(
+    { email: this.email },
+    process.env.JWT_SECRET_KEY,
+    {
+      expiresIn: process.env.JWT_EXPIRE || '1d'
+    }
+  );
+};
 
-export default mongoose.model("Doctors",doctorSchema); 
+export default mongoose.model("Doctors", doctorSchema);
